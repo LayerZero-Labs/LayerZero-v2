9175736395b7b3d281e49ab7256e038df86d7a7e
Carmen Cheng <carmencheng1030@gmail.com>
2024-01-17 08:51:17 +0800

    remove dvn adapter

--- messagelib/contracts/uln/dvn/adapters/CCIP/CCIPDVNAdapterFeeLib.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: LZBL-1.2
-
-pragma solidity ^0.8.22;
-
-import { DVNAdapterFeeLibBase } from "../DVNAdapterFeeLibBase.sol";
-
-contract CCIPDVNAdapterFeeLib is DVNAdapterFeeLibBase {}
