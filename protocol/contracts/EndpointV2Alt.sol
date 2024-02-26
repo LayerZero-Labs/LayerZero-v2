bc5396eebbb8584b13355abf750b197cd6282973
Cryptoryda <113293883+cryptoryda@users.noreply.github.com>
2024-02-26 15:57:09 +0000

    netspec

--- protocol/contracts/EndpointV2Alt.sol
+++ protocol/contracts/EndpointV2Alt.sol
@@ -7,24 +7,24 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { EndpointV2 } from "./EndpointV2.sol";
 import { Errors } from "./libs/Errors.sol";
 
-/// @notice this is the endpoint contract for layerzero v2 deployed on chains using ERC20 as native tokens
+/// @notice This is the endpoint contract for LayerZero v2 deployed on chains using ERC20 as native tokens.
 contract EndpointV2Alt is EndpointV2 {
     error LZ_OnlyAltToken();
 
-    /// @dev the altFeeToken is used for fees when the native token has no value
-    /// @dev it is immutable for gas saving. only 1 endpoint for such chains
+    /// @dev The altFeeToken is used for fees when the native token has no value.
+    /// @dev It is immutable for gas saving. Only 1 endpoint for such chains.
     address internal immutable nativeErc20;
 
     constructor(uint32 _eid, address _owner, address _altToken) EndpointV2(_eid, _owner) {
         nativeErc20 = _altToken;
     }
 
-    /// @dev handling native token payments on endpoint
-    /// @dev internal function
-    /// @param _required the amount required
-    /// @param _supplied the amount supplied
-    /// @param _receiver the receiver of the native token
-    /// @param _refundAddress the address to refund the excess to
+    /// @dev Handles native token payments on the endpoint.
+    /// @dev Internal function.
+    /// @param _required The amount required.
+    /// @param _supplied The amount supplied.
+    /// @param _receiver The receiver of the native token.
+    /// @param _refundAddress The address to refund the excess to.
     function _payNative(
         uint256 _required,
         uint256 _supplied,
@@ -35,17 +35,18 @@ contract EndpointV2Alt is EndpointV2 {
         _payToken(nativeErc20, _required, _supplied, _receiver, _refundAddress);
     }
 
-    /// @dev return the balance of the native token
+    /// @dev Returns the balance of the native token.
     function _suppliedNative() internal view override returns (uint256) {
         return IERC20(nativeErc20).balanceOf(address(this));
     }
 
-    /// @dev check if lzToken is set to the same address
+    /// @dev Checks if lzToken is set to the same address.
     function setLzToken(address _lzToken) public override onlyOwner {
         if (_lzToken == nativeErc20) revert Errors.LZ_InvalidArgument();
         super.setLzToken(_lzToken);
     }
 
+    /// @dev Returns the address of the native token.
     function nativeToken() external view override returns (address) {
         return nativeErc20;
     }
--- protocol/contracts/MessagingContext.sol
+++ protocol/contracts/MessagingContext.sol
@@ -5,14 +5,21 @@ pragma solidity ^0.8.20;
 import { IMessagingContext } from "./interfaces/IMessagingContext.sol";
 import { Errors } from "./libs/Errors.sol";
 
-/// this contract acts as a non-reentrancy guard and a source of messaging context
-/// the context includes the remote eid and the sender address
-/// it separates the send and receive context to allow messaging receipts (send back on receive())
+/**
+ * @title MessagingContext
+ * @dev This contract acts as a non-reentrancy guard and a source of messaging context.
+ *      The context includes the remote eid and the sender address.
+ *      It separates the send and receive context to allow messaging receipts (send back on receive()).
+ */
 abstract contract MessagingContext is IMessagingContext {
     uint256 private constant NOT_ENTERED = 1;
     uint256 private _sendContext = NOT_ENTERED;
 
-    /// @dev the sendContext is set to 8 bytes 0s + 4 bytes eid + 20 bytes sender
+    /**
+     * @dev Modifier to set the send context, which includes the destination eid and sender address.
+     * @param _dstEid The destination eid.
+     * @param _sender The sender address.
+     */
     modifier sendContext(uint32 _dstEid, address _sender) {
         if (_sendContext != NOT_ENTERED) revert Errors.LZ_SendReentrancy();
         _sendContext = (uint256(_dstEid) << 160) | uint160(_sender);
@@ -20,16 +27,27 @@ abstract contract MessagingContext is IMessagingContext {
         _sendContext = NOT_ENTERED;
     }
 
-    /// @dev returns true if sending message
+    /**
+     * @dev Returns true if currently sending a message.
+     * @return A boolean indicating whether sending a message.
+     */
     function isSendingMessage() public view returns (bool) {
         return _sendContext != NOT_ENTERED;
     }
 
-    /// @dev returns (eid, sender) if sending message, (0, 0) otherwise
+    /**
+     * @dev Returns the messaging context (eid, sender) if sending a message, (0, 0) otherwise.
+     * @return The messaging context as a tuple of (eid, sender).
+     */
     function getSendContext() external view returns (uint32, address) {
         return isSendingMessage() ? _getSendContext(_sendContext) : (0, address(0));
     }
 
+    /**
+     * @dev Internal function to extract the messaging context from the given context value.
+     * @param _context The context value.
+     * @return The messaging context as a tuple of (eid, sender).
+     */
     function _getSendContext(uint256 _context) internal pure returns (uint32, address) {
         return (uint32(_context >> 160), address(uint160(_context)));
     }
